641547fd3423d5b7e0a36939ad186a91d85c288f
Greased-Lightning-Tutorials <>
2021-07-12 13:20:22 +0200

    Initial commit

---
+++ LNDNodeClient/LightningHelpers/Helpers.cs
@@ -0,0 +1,41 @@
+﻿using Grpc.Core;
+using System;
+
+namespace LNDNodeClient.LightningHelpers
+{
+    public class Helpers
+    {
+        private readonly string pathToMacaroon = @" ADMIN MACAROON PATH HERE ";
+        private readonly string pathToSslCertificate = @" SSL CERTIFICATE PATH HERE";
+        private readonly string GRPCHost = " GRPC HOST HERE";
+        public Lnrpc.Lightning.LightningClient GetClient()
+        {
+            var sslCreds = GetSslCredentials();
+
+            //Create channel (Not a lightning channel but a channel to your node)
+            var channel = new Grpc.Core.Channel(GRPCHost, sslCreds);
+            var client = new Lnrpc.Lightning.LightningClient(channel);
+            return client;
+        }
+
+        public string GetMacaroon()
+        {
+            byte[] macaroonBytes = System.IO.File.ReadAllBytes(pathToMacaroon);
+            var macaroon = BitConverter.ToString(macaroonBytes).Replace("-", ""); // hex format stripped of "-" chars
+
+            return macaroon;
+        }
+
+        public SslCredentials GetSslCredentials()
+        {
+            // Due to updated ECDSA generated tls.cert we need to let gprc know that
+            // we need to use that cipher suite otherwise there will be a handshake
+            // error when we communicate with the lnd rpc server.
+            System.Environment.SetEnvironmentVariable("GRPC_SSL_CIPHER_SUITES", "HIGH+ECDSA");
+            var cert = System.IO.File.ReadAllText(pathToSslCertificate);
+            var sslCreds = new SslCredentials(cert);
+
+            return sslCreds;
+        }
+    }
+}
---
+++ LNDNodeClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Core;
+using LNDNodeClient.LightningHelpers;
+using Lnrpc;
+using System;
+
+namespace LNDNodeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var invoiceResponse = CreateInvoice();
+            Console.WriteLine(invoiceResponse.PaymentRequest);
+            Console.ReadLine();
+        }
+
+
+        public static AddInvoiceResponse CreateInvoice()
+        {
+            var helpers = new Helpers();
+            var client = helpers.GetClient();
+            var invoice = new Invoice();
+            invoice.Memo = "Satoshis Talk Post";
+            //Value in satoshis
+            invoice.Value = 1000;
+            var invoiceResponse = client.AddInvoice(invoice, new Metadata() { new Metadata.Entry("macaroon", helpers.GetMacaroon()) });
+
+            return invoiceResponse;
+        }
+    }
+}
